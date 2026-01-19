ad22368cb07cee9e315fdcb99cc67b5b0522e3a2
Noussayba <noussaibamasseoud@gmail.com>
2026-01-19 23:09:01 +0100

    adding reset button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
 import { useEffect, useState } from "react";
 import { getProducts, getRecommendations } from "./components/api";
 import ProductCard from "./components/ProductCard";
@@ -43,6 +8,11 @@ export default function App() {
   const [products, setProducts] = useState<string[]>([]);
   const [selected, setSelected] = useState("");
   const [recs, setRecs] = useState<Product[]>([]);
+
+  const handleReset = ()=> {
+    setSelected("");
+    setRecs([]);
+  };
 
   useEffect(() => {
     getProducts().then(setProducts);
@@ -60,7 +30,7 @@ export default function App() {
       </h1>
 
       <div className="flex gap-4">
-        <SearchBox products={products} onSelect={setSelected} />
+        <SearchBox products={products} value={selected} onSelect={setSelected} onReset={handleReset} />
         <button
           onClick={search}
           className="bg-black text-white px-6 rounded"
--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,21 +1,30 @@
 interface Props {
   products: string[];
+  value: string;
   onSelect: (value: string) => void;
+  onReset: ()=> void;
 }
 
-export default function SearchBox({ products, onSelect }: Props) {
+export default function SearchBox({ products, value, onSelect, onReset }: Props) {
   return (
     <>
     <input
       list="products"
       className="border p-2 w-96 rounded"
       placeholder="Search product..."
+      value ={value}
       onChange={(e) => onSelect(e.target.value)}
     />
     <datalist id="products">
       {products.map(p => <option key={p} value={p} />)}
     </datalist>
+    <button
+      type="button"
+      onClick={onReset}
+      className="ml-2 px-4 py-2 border rounded"
+    >
+      Reset
+    </button>
         </>
-
   );
 }
