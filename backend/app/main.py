3c368ee3ce049dc3143ea30647efc06c464c69e4
Noussayba <noussaibamasseoud@gmail.com>
2026-01-23 10:17:44 +0100

    add elasticsearch

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .recommender import recommend
 from .models import RecommendRequest
+from elasticsearch import Elasticsearch
+from .elasticsearch import *
+
 
 app = FastAPI(title="Product Recommendation API")
 
@@ -21,3 +24,26 @@ def products():
 @app.post("/recommend")
 def get_recommendations(req:RecommendRequest):
     return recommend(req.product)
+
+
+es = Elasticsearch("http://elasticsearch:9200")
+
+@app.on_event("startup")
+def startup():
+    create_index(es)
+    products = load_products("data/products.csv")
+    index_products(es, products)
+
+# /search?q=bluetooth+headphones
+
+@app.get("/search")
+def search_products(q: str):
+    query = {
+        "multi_match": {
+            "query": q,
+            "fields": ["title", "description", "brand"]
+        }
+    }
+
+    res = es.search(index="products", query=query)
+    return [hit["_source"] for hit in res["hits"]["hits"]]
--- backend/app/recommender.py
+++ backend/app/recommender.py
@@ -4,9 +4,16 @@ electronics = pickle.load(open("data.pkl","rb"))
 similarity = pickle.load(open("similarity.pkl","rb"))
 
 def recommend(product:str):
+    """ given a product name we first look for its index in data.pkl dataframe
+    and then fetch the corresponding cosine similarity towards other products
+    and sort and get top 10 products and their scores"""
     idx=electronics[electronics["name"]==product].index[0]
+    #.index → gets the index(es) of the matching row(s) in the DataFrame.
+    # [0] → takes the first index (assuming product exists and is unique).
     sims=list(enumerate(similarity[idx]))
-
+    #     similarity[product_index] → a row from the cosine similarity matrix
+    # This row contains similarity of this product to all products
+    # enumerate(similarity[product_index]) → pairs each similarity with the index
     top=sorted(sims,key=lambda x:x[1],reverse=True)[1:11]
 
     return [
